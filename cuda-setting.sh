c3e414aa071ba7ba6fef3b706d0f49380e770a7f
BBoBBi-MoM <shanghai110930@gmail.com>
2024-06-08 22:39:51 +0900

    .

--- cuda-setting.sh
+++ cuda-setting.sh
@@ -3,7 +3,7 @@
 sudo -v
 
 if [ $? -eq 0 ]; then
-    # cd ~/Downloads
+    cd ~/Downloads
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
     sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
     wget https://developer.download.nvidia.com/compute/cuda/12.3.1/local_installers/cuda-repo-ubuntu2204-12-3-local_12.3.1-545.23.08-1_amd64.deb
